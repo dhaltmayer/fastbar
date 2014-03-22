0f358b7dd7c82556e44bf48c3e6f0f1486f2ddfc
elliotec <elliotecweb@gmail.com>
2014-03-22 01:31:16 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,27 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.4'
+gem 'sass-rails', '~> 4.0.2'
+gem 'uglifier', '>= 1.3.0'
+gem 'coffee-rails', '~> 4.0.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+gem 'jbuilder', '~> 1.2'
+gem 'figaro'
+
+group :doc do
+  gem 'sdoc', require: false
+end
+
+group :development, :test do
+  gem 'sqlite3'
+  gem 'better_errors'
+  gem 'turn'
+  gem 'launchy'
+  gem 'binding_of_caller'
+end
+
+group :production do
+  gem 'rails_12factor'
+  gem 'pg'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Quiquor::Application.config.secret_key_base = '2793f943687d8e62a24553adba252d33bc919bfea7aad88f9fc61f530f89e1b9c15a038a406f08be593fd4ba7a30c6ae14e4f8e857a4aefb70e2fe1e7b279e92'
